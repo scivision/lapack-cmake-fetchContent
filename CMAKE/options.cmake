71f5a1b5d0c13b539bbce50484f45ecd907a127f
Michael Hirsch <scivision@users.noreply.github.com>
2021-03-05 13:11:16 -0500

    packaging
    export names like find*

--- CMAKE/options.cmake
+++ CMAKE/options.cmake
@@ -2,3 +2,10 @@ option(build_cblas "Build CBLAS" false)
 option(build_lapack "Build LAPACK" true)
 option(build_lapacke "Build LAPACKE" false)
 option(build_xblas "Build extended blas" false)
+
+set(CMAKE_EXPORT_COMPILE_COMMANDS on)
+
+# --- auto-ignore build directory
+if(NOT EXISTS ${PROJECT_BINARY_DIR}/.gitignore)
+  file(WRITE ${PROJECT_BINARY_DIR}/.gitignore "*")
+endif()
