c93e95bdc8967ba24ee6165c5cd7c4c0b1049e3e
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2021-06-16 23:35:40 -0400

    default tls on

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -4,6 +4,14 @@ option(build_lapacke "Build LAPACKE" false)
 option(build_xblas "Build extended blas" false)
 
 
+# default build all
+if(NOT DEFINED arith)
+  set(arith "s;d;c;z")
+endif()
+
+set(CMAKE_TLS_VERIFY ON)
+
+
 set(FETCHCONTENT_UPDATES_DISCONNECTED_LAPACK true)
 
 set(CMAKE_EXPORT_COMPILE_COMMANDS on)
