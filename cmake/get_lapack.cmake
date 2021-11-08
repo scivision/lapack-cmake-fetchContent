78abb0b88082210304b713405992a3d32172de60
Michael Hirsch <scivision@users.noreply.github.com>
2021-11-08 16:30:29 -0500

    lapack: allow for multiple source URLS
    this is for harmony with our build for scalapack and mumps

--- cmake/get_lapack.cmake
+++ cmake/get_lapack.cmake
@@ -1,15 +1,24 @@
 include(FetchContent)
 
-file(READ ${CMAKE_CURRENT_LIST_DIR}/lapack.json json)
+file(READ ${CMAKE_CURRENT_LIST_DIR}/libraries.json json)
 
-string(JSON lapack_url GET ${json} lapack url)
+set(lapack_urls)
+set(lapack_sha256)
+
+string(JSON N LENGTH ${json} lapack urls)
+math(EXPR N "${N}-1")
+foreach(i RANGE ${N})
+  string(JSON _u GET ${json} lapack urls ${i})
+  list(APPEND lapack_urls ${_u})
+endforeach()
+
 string(JSON lapack_sha256 GET ${json} lapack sha256)
 
 set(FETCHCONTENT_QUIET no)
 
 
 FetchContent_Declare(LAPACK
-URL ${lapack_url}
+URL ${lapack_urls}
 URL_HASH SHA256=${lapack_sha256}
 )
 
