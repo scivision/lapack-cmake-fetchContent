61788f7b7f2dced37081ff45366cf9151b600e55
Michael Hirsch <scivision@users.noreply.github.com>
2022-08-19 14:21:00 -0400

    cmake: make more programmatic

--- cmake/lapack_src.cmake
+++ cmake/lapack_src.cmake
@@ -1,10 +1,10 @@
 include(FetchContent)
 
+string(TOLOWER ${PROJECT_NAME}_src name)
+
 file(READ ${CMAKE_CURRENT_LIST_DIR}/libraries.json json)
 
 if(local)
-
-string(TOLOWER ${PROJECT_NAME}_src name)
 
 find_file(${name}_archive
 NAMES ${name}.tar.bz2 ${name}.tar.gz ${name}.tar ${name}.zip ${name}.zstd ${name}.tar.xz
@@ -18,7 +18,7 @@ endif()
 
 message(STATUS "${name}: using source archive ${${name}_archive}")
 
-FetchContent_Declare(LAPACK
+FetchContent_Declare(${PROJECT_NAME}
 URL ${${name}_archive}
 )
 
@@ -27,18 +27,18 @@ else()
 if(CMAKE_VERSION VERSION_LESS 3.19)
   include(${CMAKE_CURRENT_LIST_DIR}/Modules/JsonParse.cmake)
   sbeParseJson(meta json)
-  set(lapack_url ${meta.lapack.url})
-  set(lapack_tag ${meta.lapack.tag})
+  set(url ${meta.${name}.url})
+  set(tag ${meta.${name}.tag})
 else()
-  string(JSON lapack_url GET ${json} lapack url)
-  string(JSON lapack_tag GET ${json} lapack tag)
+  string(JSON url GET ${json} ${name} url)
+  string(JSON tag GET ${json} ${name} tag)
 endif()
 
 set(FETCHCONTENT_QUIET no)
 
-FetchContent_Declare(LAPACK
-GIT_REPOSITORY ${lapack_url}
-GIT_TAG ${lapack_tag}
+FetchContent_Declare(${PROJECT_NAME}
+GIT_REPOSITORY ${url}
+GIT_TAG ${tag}
 GIT_SHALLOW true
 INACTIVITY_TIMEOUT 60
 TLS_VERIFY true
@@ -46,4 +46,4 @@ TLS_VERIFY true
 
 endif()
 
-FetchContent_Populate(LAPACK)
+FetchContent_Populate(${PROJECT_NAME})
